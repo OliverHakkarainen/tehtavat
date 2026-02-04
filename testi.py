ff4fb659f978b8784d7b0e2461ac9564cd15021b
oliver <oliver.hakkarainen@metropolia.fi>
2026-02-04 15:19:05 +0200

    tehtava1

---
+++ testi.py
@@ -0,0 +1,30 @@
+name = input("Anna nimesi: ")
+
+print("hello" + name + "!")
+print("hello", name + "!")
+print(f"hello {name}!")
+
+age = 33
+print(f"ikäsi on:{age}")
+
+numero1 = 100
+numero2 = 200
+print(numero1 + numero2)
+print(numero1, numero2)
+
+age = input("kuinka vanha olet?")
+print(f"olet {age} vanha")
+
+age = int(input("kuinka vanha olet"))
+lisa = 10
+print(f"olet {age + lisa} vanha")
+
+luku1 = float(input("anna eka luku: "))
+luku2 = float(input("anna toka luku: "))
+print(luku1 + luku2)
+print(luku1 - luku2)
+print(luku1 * luku2)
+print(luku1 / luku2)
+print(luku1 % luku2)
+print(luku1 ** luku2)
+
---
+++ testi2.py
@@ -0,0 +1,7 @@
+pituus = int(input("anna pituutesi"))
+paino = float(input("anna paino"))
+
+bmi = paino / (pituus / 100) **2
+print("bmi:si on:",bmi)
+
+

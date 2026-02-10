123090f13e937b8631025a41891e5e490907e408
oliver <oliver.hakkarainen@metropolia.fi>
2026-02-10 12:39:52 +0200

    Valmis

--- tehtava2.py
+++ tehtava2.py
@@ -32,3 +32,21 @@ print(f"kl_keskiarvo on {kl_keskiarvo: .2f}")
 
 
 print("tehtävä_5")
+leiviska = float((input("anna leiviskat")))
+naula = float((input("anna naulat")))
+luoti = float((input("anna luodit")))
+leiviska_luoti = leiviska * 20 * 32
+naula_luoti = naula * 32
+yhteensa_luoti = luoti + naula_luoti + leiviska_luoti
+yhteensa_gramma = yhteensa_luoti * 13.3
+kilogramma = int(yhteensa_gramma /1000)
+gramma = yhteensa_gramma - (kilogramma * 1000)
+print("massa nykymittojen mukaan:")
+print(f"{kilogramma} kilogrammaa ja {gramma: .2f} grammaa")
+
+print("tehtava_6")
+import random
+koodi1 = "".join([str(random.randint(0, 9)) for _ in range(3)])
+koodi2 = "".join([str(random.randint(1, 6)) for _ in range(4)])
+print(f"Koodi 1 (3 numeroa, 0-9): {koodi1}")
+print(f"Koodi 2 (4 numeroa, 1-6): {koodi2}")
